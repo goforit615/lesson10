b1ca269646db47dbc11a4a6a59c7e7c97c29340d
goforit615 <bupark82@gsm.hs.kr>
2025-02-24 15:51:40 +0900

    interfaceex

--- src/interfaceex/Calculator.java
+++ src/interfaceex/Calculator.java
@@ -1,15 +1,14 @@
 package interfaceex;
 
-public class Calculator {
-//    public abstract class Calculator implements Calc{
-//
-//        @Override
-//        public int add(int num1, int num2) {
-//            return num1 + num2;
-//        }
-//
-//        @Override
-//        public int substract(int num1, int num2) {
-//            return num1 - num2;
-//        }
+public abstract class Calculator implements Calc{
+
+        @Override
+        public int add(int num1, int num2) {
+            return num1 + num2;
+        }
+
+        @Override
+        public int substract(int num1, int num2) {
+            return num1 - num2;
+        }
 }
--- src/interfaceex/CompleteCalc.java
+++ src/interfaceex/CompleteCalc.java
@@ -2,13 +2,13 @@ package interfaceex;
 
 public class CompleteCalc extends Calculator{
 
-    //@Override
+    @Override
     public int times(int num1, int num2) {
 
         return num1 * num2;
     }
 
-    //@Override
+    @Override
     public int divide(int num1, int num2) {
         if(num2 != 0 )
             return num1/num2;
